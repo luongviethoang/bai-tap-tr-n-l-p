fb346a2df187fd2240179061ab0500ebcb58c537
your name <hoangckhg170@gmail.com>
2021-10-12 21:08:54 +0700

    Create vd1ss10.c

---
+++ vd1ss10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int ary[3];
+	int i;
+	ary[0]=1;
+	ary[1]=2;
+	ary[2]=3;
+	ary[3]=4;
+	for(i=0;i<3;i++){
+	
+		printf("gia tri cua ary[%d] la:%d\n",i,ary[i]);}
+}								
