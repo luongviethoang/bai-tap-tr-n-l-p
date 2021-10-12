b6727a349f260eb5ba25d6e2ff1fdff5ebf317b0
your name <hoangckhg170@gmail.com>
2021-10-12 21:00:16 +0700

    Create vd5ss11.c

---
+++ vd5ss11.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int arr[2][3];
+	int row,col;
+	for(row=0;row<2;row++)
+	{
+		for(col=0;col<3;col++)
+		{
+			printf("\n enter a number at [%d][%d]",row,col);
+			scanf("%d",&arr[row][col]);
+			
+		}
+	}
+	for(row=0;row<2;row++);
+	{
+		for(col=0;col<3;col++)
+		{
+			printf("\n the number at [%d][%d]is %d",row,col,arr[row][col]);
+		}
+	}
+	return 0;
+}
